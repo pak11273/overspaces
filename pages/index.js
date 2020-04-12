332f806e28bae8feb147404bb0022c30c181d685
pak11273 <pak11273@gmail.com>
2020-04-12 13:28:13 -0500

    adds tailwind

--- pages/index.js
+++ pages/index.js
@@ -10,9 +10,10 @@ const Home = () => (
     <Navbar />
 
     <main>
-      <h1 className="title">
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
-      </h1>
+      <div className="p-4 shadow rounded bg-white">
+        <h1 className="text-purple-500 leading-normal">Next.js</h1>
+        <p className="text-gray-500">with Tailwind CSS</p>
+      </div>
     </main>
 
     <footer>
